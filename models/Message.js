f327db3524b5176eefa625f9112866ad54737d8d
Temiladejesu <otemiladejesu@gmail.com>
2026-08-14 12:07:34 +0100

    Fix User and Message models

--- models/Message.js
+++ models/Message.js
@@ -1,1 +1,9 @@
-paste Message.js code here
+const mongoose = require('mongoose');
+
+const messageSchema = new mongoose.Schema({
+  username: String,
+  text: String,
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Message', messageSchema);
--- models/User.js
+++ models/User.js
@@ -1,1 +1,15 @@
-paste User.js code here
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  username: { 
+    type: String, 
+    required: true, 
+    unique: true 
+  },
+  password: { 
+    type: String, 
+    required: true 
+  }
+}, { timestamps: true });
+
+module.exports = mongoose.model('User', userSchema);
