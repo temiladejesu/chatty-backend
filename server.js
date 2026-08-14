e78442b3f1c85094400244348c1843e6ee39ba37
Temiladejesu <otemiladejesu@gmail.com>
2026-08-14 12:56:41 +0100

    Bind to 0.0.0.0 for Render

--- server.js
+++ server.js
@@ -132,6 +132,6 @@ app.post('/api/messages', auth, async (req, res) => { // 'auth' makes it protect
     res.status(500).json({ error: err.message });
   }
 });
-server.listen(PORT, () => {
+server.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
